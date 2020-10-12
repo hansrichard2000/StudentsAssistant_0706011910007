774999b17e202b4c23b9938c865ecaf22e0d504b
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-10-12 17:03:44 +0700

    Add code to show dialog before logout

--- app/src/main/java/com/uc/studentsassistant_0706011910007/fragment/FragmentAccountActivity.java
+++ app/src/main/java/com/uc/studentsassistant_0706011910007/fragment/FragmentAccountActivity.java
@@ -2,17 +2,22 @@ package com.uc.studentsassistant_0706011910007.fragment;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -27,6 +32,7 @@ import com.uc.studentsassistant_0706011910007.R;
 import com.uc.studentsassistant_0706011910007.model.Student;
 
 public class FragmentAccountActivity extends Fragment {
+    AlphaAnimation klik = new AlphaAnimation(1F, 0.6F);
     TextView account_name, account_nim, account_email, account_gender, account_age, account_address;
     Button button_logout;
     String name, nim, email, gender, age, address;
@@ -84,11 +90,38 @@ public class FragmentAccountActivity extends Fragment {
         button_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mAuth.signOut();
-                Intent intent1 = new Intent(getActivity(), LoginActivity.class);
-                intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent1);
-                getActivity().finish();
+                view.startAnimation(klik);
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("Alert")
+                        .setIcon(R.drawable.ic_baseline_android_24)
+                        .setMessage("Are you sure you want to logout?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialog.show();
+                                new Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        dialog.cancel();
+                                        mAuth.signOut();
+                                        Intent intent1 = new Intent(getActivity(), LoginActivity.class);
+                                        intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                        startActivity(intent1);
+                                        getActivity().finish();
+                                        Toast.makeText(getActivity(), "Logout successful", Toast.LENGTH_SHORT).show();
+                                    }
+                                }, 2000);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialog.cancel();
+                            }
+                        })
+                        .create()
+                        .show();
             }
         });
     }
